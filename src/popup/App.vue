7e41c65e1ba04990f20970f39cd93e173dc0e053
Magda98 <magdakochman7@gmail.com>
2020-04-04 21:09:27 +0200

    ➕ Add vuex-persist, query-string

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,16 +1,54 @@
 <template>
-<div>
-  <hello-world />
-  XDDDD
-</div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-content>
+      <div>
+     <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+       
+      </v-btn>
+                <v-btn>
+      <v-icon>feedback</v-icon>
+    </v-btn>
+  </div>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: { HelloWorld }
+  name: 'App'
 }
 </script>
 
